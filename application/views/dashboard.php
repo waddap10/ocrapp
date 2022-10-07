23bb5dc2b53c56fcf38358ef39046903da193cc7
waddap10 <mmiftah.ridwan@gmail.com>
2022-10-07 10:14:26 +0700

    updat css

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -2,67 +2,64 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 <div class="container-fluid">
-    <div>
+    <div class="button-add">
         <form action="<?php echo base_url('/imageupload/'); ?>">
-            <input type="submit" value="Tambah Data" class="btn btn-success"/>
+            <input type="submit" value="Tambah Data" class="btn btn-success" />
         </form>
     </div>
-    <div>
-        <div class="tableFixHead">
-            <table class="table table-striped table-hover">
-                <thead>
+    <div class="table-responsive">
+        <table class="table table-hover table-bordered align-middle text-center">
+            <thead>
+                <tr class="table-dark">
+                    <th scope="col">#</th>
+                    <th scope="col">Provinsi</th>
+                    <th scope="col">Kota</th>
+                    <th scope="col">NIK</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">TTL</th>
+                    <th scope="col">Jenis Kelamin</th>
+                    <th scope="col">Alamat</th>
+                    <th scope="col">RT/RW</th>
+                    <th scope="col">Desa/Kelurahan</th>
+                    <th scope="col">Kecamatan</th>
+                    <th scope="col">Agama</th>
+                    <th scope="col">Status Kawin</th>
+                    <th scope="col">Pekerjaan</th>
+                    <th scope="col">Kewarganegaraan</th>
+                    <th scope="col">Berlaku s/d</th>
+                    <th scope="col">Golongan Darah</th>
+                    <th scope="col">Update</th>
+                    <th scope="col">Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php for ($i = 0; $i < count($dataresult); ++$i) { ?>
                     <tr>
-                        <th>#</th>
-                        <th>Provinsi</th>
-                        <th>Kota</th>
-                        <th>NIK</th>
-                        <th>Nama</th>
-                        <th>TTL</th>
-                        <th>Jenis Kelamin</th>
-                        <th>Alamat</th>
-                        <th>RT/RW</th>
-                        <th>Desa/Kelurahan</th>
-                        <th>Kecamatan</th>
-                        <th>Agama</th>
-                        <th>Status Kawin</th>
-                        <th>Pekerjaan</th>
-                        <th>Kewarganegaraan</th>
-                        <th>Berlaku s/d</th>
-                        <th>Golongan Darah</th>
-                        <th>Update</th>
-                        <th>Delete</th>
+                        <th scope="row"><?php echo ($i + 1); ?></th>
+                        <td><?php echo $dataresult[$i]->provinsi; ?></td>
+                        <td><?php echo $dataresult[$i]->kota; ?></td>
+                        <td><?php echo $dataresult[$i]->nik; ?></td>
+                        <td><?php echo $dataresult[$i]->nama; ?></td>
+                        <td><?php echo $dataresult[$i]->ttl; ?></td>
+                        <td><?php echo $dataresult[$i]->gender; ?></td>
+                        <td><?php echo $dataresult[$i]->alamat; ?></td>
+                        <td><?php echo $dataresult[$i]->rtrw; ?></td>
+                        <td><?php echo $dataresult[$i]->desa; ?></td>
+                        <td><?php echo $dataresult[$i]->kec; ?></td>
+                        <td><?php echo $dataresult[$i]->agama; ?></td>
+                        <td><?php echo $dataresult[$i]->sts_kawin; ?></td>
+                        <td><?php echo $dataresult[$i]->job; ?></td>
+                        <td><?php echo $dataresult[$i]->nation; ?></td>
+                        <td><?php echo $dataresult[$i]->exp; ?></td>
+                        <td><?php echo $dataresult[$i]->goldar; ?></td>
+                        <td><a href=<?php echo "editdata/" . $dataresult[$i]->id; ?> class="btn btn-success">Update</a></td>
+                        <td><a href=<?php echo "deletedata/" . $dataresult[$i]->id; ?> class="btn btn-danger">Delete</a></td>
                     </tr>
-                </thead>
-                <tbody>
-                    <?php for ($i = 0; $i < count($dataresult); ++$i) { ?>
-                        <tr>
-                            <td><?php echo ($i + 1); ?></td>
-                            <td><?php echo $dataresult[$i]->provinsi; ?></td>
-                            <td><?php echo $dataresult[$i]->kota; ?></td>
-                            <td><?php echo $dataresult[$i]->nik; ?></td>
-                            <td><?php echo $dataresult[$i]->nama; ?></td>
-                            <td><?php echo $dataresult[$i]->ttl; ?></td>
-                            <td><?php echo $dataresult[$i]->gender; ?></td>
-                            <td><?php echo $dataresult[$i]->alamat; ?></td>
-                            <td><?php echo $dataresult[$i]->rtrw; ?></td>
-                            <td><?php echo $dataresult[$i]->desa; ?></td>
-                            <td><?php echo $dataresult[$i]->kec; ?></td>
-                            <td><?php echo $dataresult[$i]->agama; ?></td>
-                            <td><?php echo $dataresult[$i]->sts_kawin; ?></td>
-                            <td><?php echo $dataresult[$i]->job; ?></td>
-                            <td><?php echo $dataresult[$i]->nation; ?></td>
-                            <td><?php echo $dataresult[$i]->exp; ?></td>
-                            <td><?php echo $dataresult[$i]->goldar; ?></td>
-                            <td><a href=<?php echo "editdata/".$dataresult[$i]->id;?> class="btn btn-success">Update</a></td>
-                            <td><a href=<?php echo "deletedata/".$dataresult[$i]->id;?> class="btn btn-danger">Delete</a></td>
-                        </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
-        </div>
+                <?php } ?>
+            </tbody>
+        </table>
     </div>
 </div>
-
 </body>
 
 </html>
--- application/views/readdata.php
+++ application/views/readdata.php
@@ -1,59 +1,72 @@
 <form action="<?php echo base_url('/insertdata'); ?>" method="post">
-    <div class="container">
-        <h1>Pengecekan</h1>
-        <img src="<?php echo base_url() . 'assets/images/' . $nama_image; ?>" alt="<?php echo base_url() . '/assets/images/' . $nama_image; ?>">
-        <hr>
-
-        <label for="provinsi"><b>provinsi</b></label>
-        <input type="text" name="provinsi" id="provinsi" value="<?php echo $provinsi; ?>" required>
-
-        <label for="kota"><b>kota</b></label>
-        <input type="text" name="kota" id="kota" value="<?php echo $kota; ?>" required>
-
-        <label for="nik"><b>nik</b></label>
-        <input type="text" name="nik" id="nik" value="<?php echo $nik; ?>" required>
-
-        <label for="nama"><b>nama</b></label>
-        <input type="text" name="nama" id="nama" value="<?php echo $nama; ?>" required>
-
-        <label for="ttl"><b>ttl</b></label>
-        <input type="text" name="ttl" id="ttl" value="<?php echo $ttl; ?>" required>
-
-        <label for="gender"><b>gender</b></label>
-        <input type="text" name="gender" id="gender" value="<?php echo $gender; ?>" required>
-
-        <label for="alamat"><b>alamat</b></label>
-        <input type="text" name="alamat" id="alamat" value="<?php echo $alamat; ?>" required>
-
-        <label for="rtrw"><b>rtrw</b></label>
-        <input type="text" name="rtrw" id="rtrw" value="<?php echo $rtrw; ?>" required>
-
-        <label for="desa"><b>desa</b></label>
-        <input type="text" name="desa" id="desa" value="<?php echo $desa; ?>" required>
-
-        <label for="kec"><b>kec</b></label>
-        <input type="text" name="kec" id="kec" value="<?php echo $kec; ?>" required>
-
-        <label for="agama"><b>agama</b></label>
-        <input type="text" name="agama" id="agama" value="<?php echo $agama; ?>" required>
-
-        <label for="sts_kawin"><b>sts_kawin</b></label>
-        <input type="text" name="sts_kawin" id="sts_kawin" value="<?php echo $sts_kawin; ?>" required>
-
-        <label for="job"><b>job</b></label>
-        <input type="text" name="job" id="job" value="<?php echo $job; ?>" required>
-
-        <label for="nation"><b>nation</b></label>
-        <input type="text" name="nation" id="nation" value="<?php echo $nation; ?>" required>
-
-        <label for="exp"><b>exp</b></label>
-        <input type="text" name="exp" id="exp" value="<?php echo $exp; ?>" required>
-
-        <label for="goldar"><b>goldar</b></label>
-        <input type="text" name="goldar" id="goldar" value="<?php echo $goldar; ?>" required>
+    <div class="container-fluid">
+        <div id="form-header ">
+            <h1>Pengecekan</h1>
+            <img src="<?php echo base_url() . 'assets/images/' . $nama_image; ?>" alt="<?php echo base_url() . '/assets/images/' . $nama_image; ?>">
+        </div>
 
         <hr>
+        <div class="row align-items-start">
+            <label class="col" for="provinsi"><b>provinsi</b></label>
+            <input class="col" type="text" name="provinsi" id="provinsi" value="<?php echo $provinsi; ?>" required>
+            <label class="col" for="kota"><b>kota</b></label>
+            <input class="col" type="text" name="kota" id="kota" value="<?php echo $kota; ?>" required>
+            <label class="col" for="nik"><b>nik</b></label>
+            <input class="col" type="text" name="nik" id="nik" value="<?php echo $nik; ?>" required>  
+        </div>
 
-        <input type="submit" name="tambah" value="Tambah Data" />
+        <div class="row align-items-start">
+            <label class="col" for="nama"><b>nama</b></label>
+            <input class="col" type="text" name="nama" id="nama" value="<?php echo $nama; ?>" required>
+            <label class="col" for="ttl"><b>ttl</b></label>
+            <input class="col" type="text" name="ttl" id="ttl" value="<?php echo $ttl; ?>" required>
+            <label class="col" for="gender"><b>gender</b></label>
+            <input class="col" type="text" name="gender" id="gender" value="<?php echo $gender; ?>" required>
+        </div>
+
+        <div class="row align-items-start">
+            <label class="col" for="alamat"><b>alamat</b></label>
+            <input class="col" type="text" name="alamat" id="alamat" value="<?php echo $alamat; ?>" required>
+            <label class="col" for="rtrw"><b>rtrw</b></label>
+            <input class="col" type="text" name="rtrw" id="rtrw" value="<?php echo $rtrw; ?>" required>
+            <label class="col" for="desa"><b>desa</b></label>
+            <input class="col" type="text" name="desa" id="desa" value="<?php echo $desa; ?>" required>
+        </div>
+
+        <div class="row align-items-start">
+            
+            <label class="col" for="kec"><b>kec</b></label>
+            <input class="col" type="text" name="kec" id="kec" value="<?php echo $kec; ?>" required>
+            <label class="col" for="agama"><b>agama</b></label>
+            <input class="col" type="text" name="agama" id="agama" value="<?php echo $agama; ?>" required>
+            <label class="col" for="sts_kawin"><b>sts_kawin</b></label>
+            <input class="col" type="text" name="sts_kawin" id="sts_kawin" value="<?php echo $sts_kawin; ?>" required>
+        </div>
+
+        <div class="row align-items-start">
+            <label class="col" for="job"><b>job</b></label>
+            <input class="col" type="text" name="job" id="job" value="<?php echo $job; ?>" required>
+            <label class="col" for="nation"><b>nation</b></label>
+            <input class="col" type="text" name="nation" id="nation" value="<?php echo $nation; ?>" required>
+            <label class="col" for="exp"><b>exp</b></label>
+            <input class="col" type="text" name="exp" id="exp" value="<?php echo $exp; ?>" required>
+        </div>
+
+        <div class="row align-items-start">
+            <label class="col" for="goldar"><b>goldar</b></label>
+            <input class="col" type="text" name="goldar" id="goldar" value="<?php echo $goldar; ?>" required>
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+        </div>
+        <hr>
+        <div class="row align-items-start">
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+            <input class="col btn btn-success" type="submit" name="tambah" value="Tambah Data" />
+        </div>
     </div>
 </form>
